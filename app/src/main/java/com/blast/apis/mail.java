83b9f85cf3c57ebb17e17cb2118f656f44e870ae
Khanguy <khanguy101@gmail.com>
2014-10-18 16:50:03 -0500

    basic project design structure added

---
+++ app/src/main/java/com/blast/apis/mail.java
@@ -0,0 +1,8 @@
+package com.blast.apis;
+
+/**
+ * uses java mail
+ * Created by khanguy on 10/18/14.
+ */
+public class mail {
+}
---
+++ app/src/main/java/com/blast/db.java
@@ -0,0 +1,7 @@
+package com.blast;
+
+/**
+ * Created by khanguy on 10/18/14.
+ */
+public class db {
+}
